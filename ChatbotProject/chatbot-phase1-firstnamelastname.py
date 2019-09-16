78423e9a2d3dcfe207a50a90cc8fb20f02de67a6
Jbalderas44839 <mymail@mymail.com>
2019-09-16 14:42:29 +0000

    Finished chatbot project

---
+++ ChatbotProject/chatbot-phase1-firstnamelastname.py
@@ -0,0 +1,5 @@
+'Juan Balderas'
+name = input('What is your first name? ')
+last= input('What is your last name? ')
+time = input('What time of day is it (Morning, Afternoon, Evening): ')
+print('Good' ,time , name , last [0:1])
