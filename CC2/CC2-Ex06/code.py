a97f6e7d2ab56d455fd42748c40a2fd2e001b8e9
Jbalderas44839 <mymail@mymail.com>
2019-09-20 13:10:01 +0000

    Answered question

---
+++ CC2/CC2-Ex06/code.py
@@ -0,0 +1,13 @@
+x = input('Enter score ')
+if float(x) >=1:
+    print('Bad Score')
+elif float(x) >=0.9:
+    print('A')
+elif float(x) >= 0.8:
+    print('B')
+elif float(x) >= 0.7:
+    print('C')
+elif float(x) >= 0.6:
+    print('D')
+elif float(x) <0.6:
+    print('F')
