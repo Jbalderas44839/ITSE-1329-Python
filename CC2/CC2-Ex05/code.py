952d726cf6e16645377965f2791304fa6a5c44ec
Jbalderas44839 <mymail@mymail.com>
2019-09-20 14:02:04 +0000

    Answered question

--- CC2/CC2-Ex05/code.py
+++ CC2/CC2-Ex05/code.py
@@ -1,7 +1,7 @@
 hours = input('How many hours? ')
 rate = input('Rate? ')
-if float(hours) > 40:
+if float(hours) > 40 :
    ot = (float(hours) - 40) * (float(rate) * 1.5)
    print('Pay:' , 40 * float(rate) + ot)
 else:
-    print('Pay: ' , float(hours) * float(rate))
+    print('Pay:' , float(hours) * float(rate))
