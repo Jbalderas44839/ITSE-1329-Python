dd1694df28ce87f6ac2ec28266bd482462d8b27e
Jbalderas44839 <mymail@mymail.com>
2019-09-21 18:55:32 +0000

    Answered

---
+++ CC2/CC2-Ex09/code.py
@@ -0,0 +1,10 @@
+hours = input('How many hours? ')
+rate = input('Rate? ')
+try:
+    if float(hours) > 40 :
+        ot = (float(hours) - 40) * (float(rate) * 1.5)
+        print('Pay:' , 40 * float(rate) + ot)
+    else:
+        print('Pay:' , float(hours) * float(rate))
+except:
+    print('Error, please enter numeric value ')
