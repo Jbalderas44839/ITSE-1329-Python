8fbbd305f3ff4f9252afdc7ab2b75b20a6a11192
Jbalderas44839 <mymail@mymail.com>
2019-09-25 13:27:34 +0000

    Answered questions

--- CC2/CC2-Ex16/code.py
+++ CC2/CC2-Ex16/code.py
@@ -2,8 +2,7 @@ grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 highest_so_far = -1
 
-for ________ in ________:
-    if ________ > _________:
-        ________ = ________
-
+for grades in grades:
+    if grades > highest_so_far:
+        highest_so_far = grades
 print('Highest grade is', highest_so_far)
--- CC2/CC2-Ex17/code.py
+++ CC2/CC2-Ex17/code.py
@@ -1,7 +1,9 @@
 grades = [98, 79, 85, 75, 93, 63, 82, 90, 82, 84, 74]
 
 lowest_so_far = 100
-
-# Your code goes here
+for grades in grades:
+    if grades < lowest_so_far:
+        lowest_so_far = grades
+    
 
 print('Lowest grade is', lowest_so_far)
