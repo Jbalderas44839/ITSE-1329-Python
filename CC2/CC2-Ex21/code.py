662ba0a218e14a34b50abad8770133818eac4045
Jbalderas44839 <mymail@mymail.com>
2019-10-14 13:37:42 +0000

    answered questions

--- CC2/CC2-Ex21/code.py
+++ CC2/CC2-Ex21/code.py
@@ -1,6 +1,5 @@
 #Complete code below to count number of letters
 
 fav_word = "supercalifragilisticexpialidocious"
-
+print(len(fav_word))
 # Your code below
-
--- CC2/CC2-Ex22/code.py
+++ CC2/CC2-Ex22/code.py
@@ -1,5 +1,5 @@
 #Complete code below to count number of letters
 
 fav_word = "supercalifragilisticexpialidocious"
-
+print(len(fav_word))
 # Your code below
